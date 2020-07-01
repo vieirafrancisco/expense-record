a7b013ea482cc34aa81a41833ab13b465d3f2524
Francisco <fvcneto.master98@gmail.com>
2020-06-30 21:40:53 -0300

    initial register logic

--- invoice_register/main_app/views.py
+++ invoice_register/main_app/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
     return render(request, 'main_app/index.html')
+
+def register(request):
+    error_message = ''
+    if request.POST:
+        try:
+            args = {key:value for key, value in request.POST.items() if 'csrf' not in key}
+            user = User(**args)
+            user.save()
+        except Exception as e:
+            error_message = e
+        else:
+            return HttpResponseRedirect('main_app/index.html', 
+                                        {'username': request.POST['username']})
+    return render(request, 'main_app/register.html', {'error_message': error_message})
+
+def login(request):
+    return HttpResponse("Hello Login!")
