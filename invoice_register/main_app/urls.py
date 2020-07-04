b10f22d77bb2580035f36094f426f12f33ef3ecd
Francisco <fvcneto.master98@gmail.com>
2020-07-04 11:21:16 -0300

    rename login function

--- invoice_register/main_app/urls.py
+++ invoice_register/main_app/urls.py
@@ -6,7 +6,7 @@ app_name = 'main_app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
+    path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('<int:user_id>/dashboard/', views.dashboard, name='dashboard')
 ]
--- invoice_register/main_app/views.py
+++ invoice_register/main_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login as auth_login, logout
+from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 
 # Create your views here.
@@ -18,7 +18,7 @@ def dashboard(request, user_id):
         return Http404("User not logged in!")
     return render(request, 'main_app/dashboard.html', {'user': user})
 
-def login(request):
+def login_view(request):
     error_message = ''
     if request.POST:
         args = request.POST
@@ -26,7 +26,7 @@ def login(request):
         if user is not None:
             if user.is_active:
                 request.session.set_expiry(86400)
-                auth_login(request, user)
+                login(request, user)
             return HttpResponseRedirect(reverse('main_app:dashboard', args=(user.id,)))
         error_message = "User doesn't exist!"
     if request.user.is_authenticated:
