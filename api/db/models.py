c9cf006d4dce3179e15d7bace6d2b3d25030ef93
Francisco Neto <fvcneto.master98@gmail.com>
2020-07-27 23:03:36 -0300

    create new methods to manipulate User model

--- api/db/models.py
+++ api/db/models.py
@@ -1,5 +1,8 @@
 from abc import ABC, abstractmethod
 
+from exceptions.db_exceptions import ModelInstanceAlreadyExist
+from db.sqlite_db import db
+
 class Model(ABC):
     def __init__(self, table_name):
         self.table_name = table_name
@@ -16,10 +19,35 @@ class Model(ABC):
 
 class User(Model):
     def __init__(self, name, email, income):
-        Model.__init__(self, 'user')
+        Model.__init__(self, 'users')
         self.name = name
         self.email = email
         self.income = income
+        self.attr_names = ["name", "email", "income"]
     
+    def __repr__(self):
+        return f"Model<{self.table_name}: {self.email}>"
+
     def save(self):
+        try:
+            db.insert_into_table(
+                self.table_name, 
+                self.attr_names, 
+                [self.name, self.email, self.income]
+            )
+        except:
+            raise ModelInstanceAlreadyExist
+        
+    @classmethod
+    def get_all(cls):
+        users_tuples = db.select_all_from('users', ["name", "email", "income"])
+        for user_tuple in users_tuples:
+            yield User(*user_tuple)
+
+    @classmethod
+    def get_one(cls):
+        ...
+
+    @classmethod
+    def filter_by_email(cls):
         ...
