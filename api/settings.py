5dfcf975131ef687eb2b5b6728d6b900d89890ab
Francisco Neto <fvcneto.master98@gmail.com>
2020-07-30 19:44:44 -0300

    add file and path from database

--- api/settings.py
+++ api/settings.py
@@ -4,4 +4,7 @@ import os
 DEBUG = True
 
 # database
-DB_PATH = os.path.join("api", "rsc", "db", "database.db")
+DB_NAME = "database.db"
+DB_PATH = os.path.join("rsc", "db")
+DB_FILE_PATH = os.path.join(DB_PATH, DB_NAME)
+TABLES_JSON_PATH = os.path.join("rsc", "sql", "tables.json")
