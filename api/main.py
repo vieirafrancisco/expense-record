209457d371a2983a57fee5a2dcc7f26a54bd2cc2
Francisco Neto <fvcneto.master98@gmail.com>
2020-07-30 23:25:28 -0300

    add heroku PORT environ to the application

--- api/main.py
+++ api/main.py
@@ -1,3 +1,4 @@
+import os
 from src.routes import app
 
 from settings import DEBUG
@@ -5,4 +6,5 @@ from scripts.setup import setup_run
 
 if __name__ == '__main__':
     setup_run()
-    app.run(debug=DEBUG)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', debug=DEBUG, port=port)
