92915af263144599c78f787c7470d6cbd3a98f52
Francisco Neto <fvcneto.master98@gmail.com>
2020-08-10 18:00:59 -0300

    create dictionary to store postgresql credentials

--- api/settings.py
+++ api/settings.py
@@ -1,4 +1,6 @@
 import os
+import subprocess
+from urllib.parse import urlparse
 
 # dev
 DEBUG = False
@@ -8,3 +10,15 @@ DB_NAME = "database.db"
 DB_PATH = os.path.join("rsc", "db")
 DB_FILE_PATH = os.path.join(DB_PATH, DB_NAME)
 TABLES_JSON_PATH = os.path.join("rsc", "sql", "tables.json")
+
+DATABASE_URL = os.environ.get(
+    "DATABASE_URL",
+    subprocess.check_output(["heroku", "config:get", "DATABASE_URL"]).decode().replace('\n', '')
+)
+parse = urlparse(DATABASE_URL)
+POSTGRESQL_CREDENTIALS = {
+    "host": parse.hostname,
+    "database": parse.path[1:],
+    "user": parse.username,
+    "password": parse.password
+}
