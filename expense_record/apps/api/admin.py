8529c0ec0de7773e37cb85231666a0405508a25c
Francisco Neto <fvcneto.master98@gmail.com>
2021-09-15 19:53:21 -0300

    feat: create Bill model

--- expense_record/apps/api/admin.py
+++ expense_record/apps/api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import CreditCard
+from .models import CreditCard, Bill
 
 admin.site.register(CreditCard)
+admin.site.register(Bill)
--- expense_record/apps/api/models.py
+++ expense_record/apps/api/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -14,4 +16,39 @@ class CreditCard(models.Model):
         unique_together = (("user", "name"),)
 
     def __str__(self):
-        return f"CreditCard ({self.name})"
+        return f"Cartão de Crédito ({self.name})"
+
+
+class Bill(models.Model):
+    credit_card = models.ForeignKey(CreditCard, on_delete=models.CASCADE, verbose_name="Cartão de Crédito")
+    name = models.CharField(max_length=255, verbose_name="Nome")
+    created_at = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(verbose_name="Data")
+    value = models.DecimalField(verbose_name="Valor", decimal_places=2, max_digits=19)
+    is_service = models.BooleanField(default=False, verbose_name="É um serviço?")
+    end_service_date = models.DateTimeField(null=True, blank=True, verbose_name="Data de finalização do Serviço")
+    is_installment = models.BooleanField(default=False, verbose_name="É parcelado?")
+    installment_count = models.SmallIntegerField(default=0, verbose_name="Número de parcelas")
+    has_promotion = models.BooleanField(default=False, verbose_name="Tem promoção?")
+    promotion_count = models.SmallIntegerField(default=0, verbose_name="Número de mêses com a promoção")
+
+    class Meta:
+        verbose_name = "Conta"
+        verbose_name_plural = "Contas"
+        constraints = [
+            models.CheckConstraint(
+                check=~(models.Q(is_service=True) & models.Q(is_installment=True)),
+                name="Service and Installmente can't both be True."
+            ),
+        ]
+
+    def __str__(self):
+        return f"Conta ({self.name}) do {self.credit_card}"
+    
+    @property
+    def promotion_due_date(self):
+        if self.has_promotion:
+            day = self.date.day
+            month = (self.date.month + self.promotion_count) % 12
+            year = self.date.year + (self.date.month + self.promotion_count) // 12
+            return date(day=day, month=month, year=year)
